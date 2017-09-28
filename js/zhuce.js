14c038fd20df03a0fc8d14f8e9e7703a720d6985
haitao <895746448@qq.com>
2017-09-28 22:03:45 +0800

    第二次

--- js/zhuce.js
+++ js/zhuce.js
@@ -29,7 +29,30 @@ w$("txt1").onblur = function(){
 			return;
 	}
 };
-
+//	图片验证码
+	var img=["img/yzm1 (1).png","img/yzm1 (2).png","img/yzm1 (3).png","img/yzm1 (4).png","img/yzm1 (5).png","img/yzm1 (6).png","img/yzm1 (7).png","img/yzm1 (8).png","img/yzm1 (9).png","img/yzm1 (10).png","img/yzm1 (11).png","img/yzm1 (12).png","img/yzm1 (13).png","img/yzm1 (14).png","img/yzm1 (15).png","img/yzm1 (16).png","img/yzm1 (17).png","img/yzm1 (18).png","img/yzm1 (19).png","img/yzm1 (20).png"]
+	var imgz=["NCKN","BXUA","3WCW","PW8E","H2S8","75CN","7V8R","V6SP","SXFE","UH5F","MMD3","BPA7","MRT9","4P8E","WZAW","PSE6","9HBN","FWYT","FP2D","2WTN"];
+		var num=parseInt(Math.random()*20)
+		imgz.index=num;
+		console.log(imgz[imgz.index])
+	imga=document.getElementsByClassName("imgs3")[0].src=img[num];
+		w$("imgs").onclick=function(){
+			var num=parseInt(Math.random()*20)
+			imga=document.getElementsByClassName("imgs3")[0].src=img[num];
+		}
+		w$("txt2").onblur=function(){
+			var sum=w$("txt2").value;
+			if(sum!=imgz[imgz.index]){
+				w$("spans0").innerHTML="验证码输入错误";
+				w$("spans0").style.cssText="color:red";
+				w$("imgs20").style.opacity="0";
+			}else{
+				w$("imgs20").style.opacity="1";
+				w$("spans0").innerHTML="验证码输入正确";
+				w$("spans0").style.cssText="color:green";
+			} 
+		}
+	
 //随机验证码
 	var arr=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o',
 	'p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9','0'];
@@ -72,39 +95,7 @@ w$("txt1").onblur = function(){
 		} 
 	}
 	
-	
-	
-	
-	var img=["img/yzm1 (1).png","img/yzm1 (2).png","img/yzm1 (3).png","img/yzm1 (4).png","img/yzm1 (5).png","img/yzm1 (6).png",]
-	
-	for(let i=0;i<img.length;i++){
-		photo=img[i];
-			console.log(photo)
-		w$("imgs").onclick=function(){
-			//this.index=i;
-			//var imga=document.createElement("img")
-			//w$("imgs").appendChild(imga)
-			//w$("imgs").innerHTML=img[i]
-			
-			imga=document.getElementsByClassName("imgs3")[0].setAttribute('src','img/yzm1 (10).png')
-			
-		}
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 //密码
 	w$("txt5").onblur=function(){
 	//密码只能使用数字字母下划线，长度不得小于8位
@@ -134,12 +125,18 @@ w$("txt1").onblur = function(){
 	}
 	
 w$("zhuce").onclick = function(){
-	if((w$("txt1").value.length<=0) || (w$("txt3").value.length<=0) || (w$("txt5").value.length<=0) || (w$("txt6").value.length<=0)){
+	if((w$("txt1").value.length<=0)|| (w$("txt2").value.length<=0) || (w$("txt3").value.length<=0) || (w$("txt5").value.length<=0) || (w$("txt6").value.length<=0)){
 		w$("h6").innerHTML = "请检查您的页面信息是否填写完整";
 	}else{
 		let str = w$("txt1").value;
 		let reg = /^1\d{10}$/;
 		if(reg.test(str)==true){
+			var sum=w$("txt2").value;
+			if(sum!=imgz[imgz.index]){
+				w$("spans0").innerHTML="图片验证码输入错误";
+				w$("spans0").style.cssText="color:red";
+				w$("imgs20").style.opacity="0";
+			}else{
 			let num=w$("txt3").value;
 			if(num!=w$("txt4").value){
 				w$("spans1").innerHTML="验证码输入错误";
@@ -169,6 +166,7 @@ w$("zhuce").onclick = function(){
 					type:"post",
 					success:function(data){
 								if(data=="1"){
+									alert("恭喜您，您已注册成功")
 									window.location.href="index.html";
 								}else{
 									alert("您好，您的用户名已经被占用了，请重新注册");
@@ -192,9 +190,9 @@ w$("zhuce").onclick = function(){
 			}
 		}
    }
-			
+	}		
 		}else{
-			w$("qa5a").innerHTML = "您好，请检查您的邮箱号或手机号格式是否正确";
+			w$("qa5a").innerHTML = "您好，请检查您的手机号格式是否正确";
 		}
 	}
 }
